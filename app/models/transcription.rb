28ad32b2df62a6edac377736d55596c85ad06157
Harsh <harshvijayvargiya21@gmail.com>
2025-11-29 16:25:23 +0530

    Added Annotate gem

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: transcriptions
+#
+#  id             :bigint           not null, primary key
+#  audio_filename :string
+#  duration       :integer
+#  raw_text       :text
+#  status         :string
+#  summary_text   :text
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Transcription < ApplicationRecord
+  validates :audio_filename, presence: true
 end
